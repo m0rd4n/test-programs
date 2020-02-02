1baf02c08a4d0b1b74110432108399a8198a9faa
Max Rupprecht <m.rpprcht@protonmail.com>
2020-02-02 15:52:02 +0100

    Update raffle.py

--- raffle.py
+++ raffle.py
@@ -1,9 +1,37 @@
-import random
-def create_list(liste):
-	f=open(liste,"r")
-	return f.readlines()
-file=str(input("Which file should be opened?"))
-objects=create_list(file)
-rndm=random.randint(0,len(objects)-1)
-choice=objects[rndm]
-print("The choice is: \033[1;31;40m", choice)
+from tkinter import *
+from tkinter import messagebox
+from random import choice
+
+items = []
+root = Tk()
+root.title("Raffle")
+root.geometry("208x290")
+
+item = Entry(root)
+item.grid(row=0, column=0, pady=30)
+
+
+def add():
+    if item.get() == "":
+        return
+    items.append(item.get())
+    item_list.insert(END, item.get())
+    item.delete(0, END)
+
+
+addButton = Button(root, text="add", command=add)
+addButton.grid(row=0, column=1, pady=30)
+
+item_list = Listbox(root)
+item_list.grid(row=2, column=0, pady=30)
+
+
+def raffle():
+    rndm_item = choice(items)
+    messagebox.showinfo("", f"{rndm_item}")
+
+
+raffleButton = Button(root, text="choose item", command=raffle)
+raffleButton.grid(row=2, column=1, pady=30)
+
+root.mainloop()
